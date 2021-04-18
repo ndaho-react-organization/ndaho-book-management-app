9104f7b1868568f51758d37199bdca8257e08fc6
ndayiho <ndayiho5@gmail.com>
2021-04-18 19:37:09 +0200

    intro

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import AppRouter from './router/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
