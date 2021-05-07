4fc88326d31a982d4b256f7291d36350ef4e93aa
ndayiho <ndayiho5@gmail.com>
2021-05-07 21:22:17 +0200

    Use React's Context API

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import BookForm from './BookForm';
+import BooksContext from '../context/BooksContext';
 
-const AddBook = ({ history, books, setBooks }) => {
+const AddBook = ({ history }) => {
+  
+  const { books, setBooks } = useContext(BooksContext);
+
   const handleOnSubmit = (book) => {
     setBooks([book, ...books]);
     history.push('/');
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -5,43 +5,28 @@ import AddBook from '../components/AddBook';
 import EditBook from '../components/EditBook';
 import BooksList from '../components/BooksList';
 import useLocalStorage from '../hooks/useLocalStorage';
+import BooksContext from '../context/BooksContext';
 
 const AppRouter = () => {
     const [books, setBooks] = useLocalStorage('books', []);
-
+  
     return (
-        <BrowserRouter>
-            <div>
-                <Header />
-                <div className="main-content">
-                    <Switch>
-                        <Route
-                            render={(props) => (
-                                <BooksList {...props} books={books} setBooks={setBooks} />
-                            )}
-                            path="/"
-                            exact
-                        />
-                        <Route
-                            render={(props) => (
-                                <AddBook {...props} books={books} setBooks={setBooks} />
-                            )}
-                            path="/add"
-                        />
-
-                        <Route
-                            render={(props) => (
-                                <EditBook {...props} books={books} setBooks={setBooks} />
-                            )}
-                            path="/edit/:id"
-                        />
-
-                        <Route component={() => <Redirect to="/" />} />
-                    </Switch>
-                </div>
-            </div>
-        </BrowserRouter>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <div className="main-content">
+            <BooksContext.Provider value={{ books, setBooks }}>
+              <Switch>
+                <Route component={BooksList} path="/" exact={true} />
+                <Route component={AddBook} path="/add" />
+                <Route component={EditBook} path="/edit/:id" />
+                <Route component={() => <Redirect to="/" />} />
+              </Switch>
+            </BooksContext.Provider>
+          </div>
+        </div>
+      </BrowserRouter>
     );
-};
-
+  };
+  
 export default AppRouter;
